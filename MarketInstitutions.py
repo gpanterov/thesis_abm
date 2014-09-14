e811ebeef9ff122cfc3e884979407efc6a8c1d66
George <gpanterov@g-computer.(none)>
2014-09-13 21:34:49 -0400

    Added utility function to Simple Trader

--- MarketInstitutions.py
+++ MarketInstitutions.py
@@ -7,50 +7,83 @@ from scipy.optimize import minimize
 import statsmodels.api as sm
 
 
+def log_util(trade_type, P, prob,  X=1., v1=1., v2=0.):
+	if trade_type == "buy":
+		return prob * np.log(X + v1 - P) + (1 - prob) * np.log(X + v2 - P)
+	elif trade_type == "sell":
+		return prob * np.log(X + P - v1) + (1 - prob) * np.log(X + P - v2)
+	else:
+		return np.log(X)
 
 class SimpleTrader(object):
-	def __init__(self, market, mu):
+	def __init__(self, market, prob):
 		self.market = market
-		self.mu = mu
+		self.prob = prob
 		self.own_trades = []
 
 	def create_trade(self):
-		p = self.market.get_last_price()
-		if self.mu > p:
-			# Buy!
+		P = self.market.get_last_price()
+
+		util_buy = log_util("buy", P, self.prob)
+		util_sell = log_util("sell", P, self.prob)
+		util_none = log_util(None, P, self.prob)
+		if util_buy > util_none:
+			# Buy
 			x = 1.
-		else:
+		elif util_sell > util_none:
 			x = -1.
+		else:
+			return None
 		self.own_trades.append(x)
 		self.market.submit_trade(x)
+
+#		if self.mu > p:
+#			# Buy!
+#			x = 1.
+#		else:
+#			x = -1.
+#		self.own_trades.append(x)
+#		self.market.submit_trade(x)
+
+	def update_expectations(self):
+		pass
+
+
+				
+
 
 class SimpleMarket(object):
 	def __init__(self, price_history):
 		self.price_history = price_history
 		self.inventory = 0
+		self.inventory_history = []
 
 	def get_last_price(self):
 		return self.price_history[-1]
 
 	def submit_trade(self, x):
 		self.inventory += - x
-		if np.abs(self.inventory) > 10:  # Update price if inventory grows too much
+		self.inventory_history.append(self.inventory)
+
+		if np.abs(self.inventory) > 5:  # Update price if inventory grows too much
 			self.update_price()
 
 
 	def update_price(self, g=0.01):
 		p = self.get_last_price()
+		inventory_grow = np.abs(self.inventory_history[-1]) > np.abs(self.inventory_history[-2])
 
-		if self.inventory > 0:
+		if self.inventory > 0 and inventory_grow:
 			# Positive inventory means a lot of sellers - price should go down
 			new_price = (1 - g) * p
-		else:
+			self.price_history.append(new_price)
+		elif self.inventory < 0 and inventory_grow:
 			# Negative inventory means a lot of buyers- prices go up
 			new_price = (1 + g) * p
-
-		self.price_history.append(new_price)
-
+			self.price_history.append(new_price)
 			
+		else:
+			return None
 
 class SimplePopulation(object):
 
@@ -81,4 +114,4 @@ class SimplePopulation(object):
 	def trade(self):
 		for t in self.traders:
 			t.create_trade()
-
+			#print self.market.get_last_price(), self.market.inventory
