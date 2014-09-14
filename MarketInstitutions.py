635869e2834ea1e37129ffe884b1cb5f4b4b4b77
George <gpanterov@g-computer.(none)>
2014-09-14 02:59:22 -0400

    fixed some issues and tested new stuff

--- MarketInstitutions.py
+++ MarketInstitutions.py
@@ -5,6 +5,34 @@ Script contains the market institutions for the market simulation
 import numpy as np
 from scipy.optimize import minimize
 import statsmodels.api as sm
+import random
+from bisect import bisect
+
+def cdf(weights):
+	total=sum(weights) * 1.
+	result=[]
+	cumsum=0
+	for w in weights:
+		cumsum += w 
+		result.append(cumsum/total)
+	return result
+
+def choice(population, cdf_vals):
+	"""
+	Returns a random element of population sampled according
+	to the weights cdf_vals (produced by the func cdf)
+	Inputs
+	------
+	population: list, a list with objects to be sampled from
+	cdf_vals: list/array with cdfs (produced by the func cdf)
+	Returns
+	-------
+	An element from the list population
+	"""
+	assert len(population) == len(cdf_vals)
+	x = random.random()
+	idx = bisect(cdf_vals,x)
+	return population[idx]
 
 
 def log_util(trade_type, P, prob,  X=1., v1=1., v2=0.):
@@ -27,10 +55,12 @@ def exp_util(trade_type, P, prob, a=1e-3, X=1., v1=1., v2=0.):
 		return 1 - np.exp(-a * X)
 
 class SimpleTrader(object):
-	def __init__(self, market, prob):
+	def __init__(self, market, prob, pop_size):
 		self.market = market
 		self.prob = prob
 		self.own_trades = []
+		self.pop_size = pop_size
+		self.get_trade_incentive(exp_util)
 
 	def get_trade_incentive(self, util_func):
 		P = self.market.get_last_price()
@@ -38,21 +68,22 @@ class SimpleTrader(object):
 		self.util_sell = util_func("sell", P, self.prob)
 		self.util_none = util_func(None, P, self.prob)
 
-		self.trade_incentive = np.max((self.util_buy, self.util_sell, self.util_none))
-
+		self.trade_incentive = np.max((self.util_buy, self.util_sell, self.util_none)) / \
+															 self.util_none - 1
+		return self.trade_incentive
 	def create_trade(self):
 		self.get_trade_incentive(exp_util)
 
-		if self.util_buy > self.util_none:
+		if self.util_buy == (self.trade_incentive + 1) * self.util_none:
 			# Buy
 			x = 1.
-		elif self.util_sell > self.util_none:
+		elif self.util_sell == (self.trade_incentive + 1) * self.util_none:
 			x = -1.
 		else:
 			return None
 		self.own_trades.append(x)
 		self.market.submit_trade(x)
-		print x
+
 	def update_expectations(self, n=10):
 		prices = np.array(self.market.price_history)
 
@@ -62,6 +93,8 @@ class SimpleTrader(object):
 			price_change = prices[1:] - prices[:-1]
 			num_hikes = 1. * np.sum(price_change[-n:] > 0) / n
 			self.prob = np.random.binomial(1, num_hikes)
+			if np.sum(price_change[-n:]) == 0:
+				self.prob = np.random.binomial(1, 0.5)
 
 class SimpleMarket(object):
 	def __init__(self, price_history, max_price=0.95, min_price=0.05):
@@ -78,8 +111,10 @@ class SimpleMarket(object):
 		self.inventory += - x
 		self.inventory_history.append(self.inventory)
 
-		if np.abs(self.inventory) > 5:  # Update price if inventory grows too much
+		if np.abs(self.inventory) > 1:  # Update price if inventory grows too much
 			self.update_price()
+		else:
+			self.price_history.append(self.get_last_price())
 
 
 	def update_price(self, g=0.01):
@@ -94,7 +129,7 @@ class SimpleMarket(object):
 			new_price = (1 + g) * p
 			
 		else:
-			return None
+			new_price = p
 		new_price = np.max((self.min_price, new_price))
 		new_price = np.min((self.max_price, new_price))
 		self.price_history.append(new_price)
