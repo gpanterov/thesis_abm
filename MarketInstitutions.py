709b69750093b612b55b42af011b2ae60df064a5
George <gpanterov@g-computer.(none)>
2014-09-14 01:10:32 -0400

    added update expectations to Trader class

--- MarketInstitutions.py
+++ MarketInstitutions.py
@@ -15,22 +15,33 @@ def log_util(trade_type, P, prob,  X=1., v1=1., v2=0.):
 	else:
 		return np.log(X)
 
+def exp_util(trade_type, P, prob, a=1e-3, X=1., v1=1., v2=0.):
+	if trade_type == "buy":
+		return prob * ( 1 - np.exp(- a * (X + v1 - P))) + \
+					(1 - prob) * (1 - np.exp(- a * (X + v2 - P)))
+	elif trade_type == "sell":
+		return prob * ( 1 - np.exp(- a * (X + P - v1))) + \
+					(1 - prob) * (1 - np.exp(- a * (X + P - v2)))
+
+	else:
+		return 1 - np.exp(-a * X)
+
 class SimpleTrader(object):
 	def __init__(self, market, prob):
 		self.market = market
 		self.prob = prob
 		self.own_trades = []
 
-	def get_trade_incentive(self):
-
+	def get_trade_incentive(self, util_func):
 		P = self.market.get_last_price()
-		self.util_buy = log_util("buy", P, self.prob)
-		self.util_sell = log_util("sell", P, self.prob)
-		self.util_none = log_util(None, P, self.prob)
+		self.util_buy = util_func("buy", P, self.prob)
+		self.util_sell = util_func("sell", P, self.prob)
+		self.util_none = util_func(None, P, self.prob)
 
 		self.trade_incentive = np.max((self.util_buy, self.util_sell, self.util_none))
+
 	def create_trade(self):
-		self.get_trade_incentive()
+		self.get_trade_incentive(exp_util)
 
 		if self.util_buy > self.util_none:
 			# Buy
@@ -41,13 +52,16 @@ class SimpleTrader(object):
 			return None
 		self.own_trades.append(x)
 		self.market.submit_trade(x)
+		print x
+	def update_expectations(self, n=10):
+		prices = np.array(self.market.price_history)
 
-	def update_expectations(self):
-		pass
-
-
-				
-
+		if len(prices) < 3:
+			self.prob = np.random.binomial(1, 0.5)
+		else:
+			price_change = prices[1:] - prices[:-1]
+			num_hikes = 1. * np.sum(price_change[-n:] > 0) / n
+			self.prob = np.random.binomial(1, num_hikes)
 
 class SimpleMarket(object):
 	def __init__(self, price_history, max_price=0.95, min_price=0.05):
